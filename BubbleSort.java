bce2859536e559d55b6bb76cb49dc2738bece81b
Rakesh <rakesh400211@gmail.com>
2024-02-09 22:15:27 +0530

    bubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of values : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" values : ");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Before sorting : ");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+        int temp;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            System.out.println("");
+            for (int a : arr) {
+                System.out.print(a + " ");
+            }
+        }
+        System.out.println("");
+        System.out.println("After sorting : ");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+
+    }
 }
